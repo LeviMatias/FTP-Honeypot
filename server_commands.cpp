4a124d337e459f739990c447609abfc54959731d
Matias <matilevi35@gmail.com>
2019-10-01 04:55:35 -0300

    forgot to add base command to cpp

--- server_commands.cpp
+++ server_commands.cpp
@@ -5,6 +5,19 @@
 #include "server_commands.h"
 
 
+std::vector<Message>
+Command::Execute(SafeSet<std::string> &dirs, Config &conf, UserProfile &user,
+                 std::string args) {
+    std::vector<Message>m;
+    m.emplace_back(args,true);
+    return m;
+}
+
+void Command::AssertLogged(UserProfile &user) {
+    if (!user.IsLogged()) throw NotLoggedException();
+}
+
+
 ///////////////////////////// SYST ////////////////////////////
 std::vector<Message>
 SystCmd::Execute(SafeSet<std::string> &dirs, Config &conf, UserProfile &user,
