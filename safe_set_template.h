80a34264d344bc94d842fac59d9d3d0f203e2691
Matias <matilevi35@gmail.com>
2019-10-09 02:23:50 -0300

    Revert "moved files to folder" safe set now uses a single mutex (yes read is thread safe but its ok for now)

--- safe_set_template.h
+++ safe_set_template.h
@@ -7,45 +7,29 @@
 
 #include <set>
 #include <mutex>
-#include <condition_variable>
 #include <vector>
 #include <algorithm>
 
 template <class T>
 class SafeSet{
 public:
-SafeSet<T>();
 //SafeSet allows reading from multiple threads but locks the set when
 //its being written to
 
-bool Insert(T elem);
+    bool Insert(T elem);
 
-bool Erase(T elem);
+    bool Erase(T elem);
 
 //POS returns a list containing all elements inside the set
-std::vector<T> GetAll();
+    std::vector<T> GetAll();
 
-bool Contains(T elem);
+    bool Contains(T elem);
 
 
 private:
     std::set<T> set;
     std::mutex m;
-    std::mutex m2;
-    bool locked_for_read{};
-    bool locked_for_write{};
-    std::condition_variable cv_unlock_read;
-    std::condition_variable cv_unlock_write;
-
-    void LockSet();
 };
-
-template<class T>
-SafeSet<T>::SafeSet() {
-    this->locked_for_read = false;
-    this->locked_for_write = false;
-}
-
 
 template<class T>
 bool SafeSet<T>::Insert(T elem) {
@@ -62,7 +46,6 @@ bool SafeSet<T>::Insert(T elem) {
 template<class T>
 bool SafeSet<T>::Erase(T elem) {
     std::unique_lock<std::mutex> lock(this->m);
-    this->locked_for_read = true;
 
     auto it = set.find(elem);
     bool was_found = it != set.end();
@@ -77,10 +60,8 @@ bool SafeSet<T>::Erase(T elem) {
 
 template<class T>
 std::vector<T> SafeSet<T>::GetAll() {
+    std::unique_lock<std::mutex> lock(m);
     std::vector<T> elems;
-    if (locked_for_read){
-        LockSet();
-    }
     std::for_each(set.begin(),set.end(),[&](T elem){
         elems.emplace_back(elem);
     });
@@ -89,19 +70,9 @@ std::vector<T> SafeSet<T>::GetAll() {
 
 template<class T>
 bool SafeSet<T>::Contains(T elem) {
-    if (locked_for_read){
-        LockSet();
-    }
+    std::unique_lock<std::mutex> lock(m);
     auto it = this->set.find(elem);
     return (it != this->set.end());
-}
-
-template<class T>
-void SafeSet<T>::LockSet() {
-    std::unique_lock<std::mutex> lock(this->m2);
-    while(this->locked_for_read){
-        this->cv_unlock_read.wait(lock);
-    }
 }
 
 
